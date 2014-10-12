022b4d7f6e1e922c7077b22f8b03377cb98eacda
aumathew <aumathew@microsoft.com>
2014-10-11 23:14:17 -0700

    Commit to local
    Initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ETagPoller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
